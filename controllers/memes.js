ec9fa019bb7d3df58207a7ffa4b8a247e1a541be
Primemerlinian <mwatson0518@gmail.com>
2023-02-25 23:16:23 -0500

    fix create function

--- controllers/memes.js
+++ controllers/memes.js
@@ -1,14 +1,14 @@
 const { Meme } = require ('../models')
-const cloudinary = require('cloudinary').v2
+// const cloudinary = require('cloudinary').v2
 
 
 async function create(req, res) {
   try {
-    const meme = await Meme.create(req.body)
-    res.status(200).json(meme)
+    const meme = await Meme.create({ profileId: req.user.id, ...req.body });
+    res.status(200).json(meme);
   } catch (error) {
-    console.log(error)
-    res.status(500).json({ err: error })
+    console.log(error);
+    res.status(500).json({ err: error });
   }
 }
 
